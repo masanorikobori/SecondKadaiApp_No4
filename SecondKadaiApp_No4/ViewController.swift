511a83b1db2926378fe303cb652b9e2a60d27fe1
masanori.kobori <livephotobank@yahoo.co.jp>
2019-08-24 17:50:50 +0900

    Initial Commit

---
+++ SecondKadaiApp_No4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp_No4
+//
+//  Created by Kobori on 2019/08/24.
+//  Copyright © 2019 Masanori Kobori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
